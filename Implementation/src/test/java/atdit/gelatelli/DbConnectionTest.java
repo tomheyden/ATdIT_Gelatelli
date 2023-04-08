3d7e1dd03b3ab430f256224f022efb768edbd00c
tomheyden <Tom.Heyden@gmx.de>
2023-04-08 13:39:42 +0200

    Unit Tests updated - Dependency added

--- Implementation/src/test/java/atdit/gelatelli/DbConnectionTest.java
+++ Implementation/src/test/java/atdit/gelatelli/DbConnectionTest.java
@@ -1,5 +1,22 @@
 package java.atdit.gelatelli;
 
-public class DbConnectionTest{
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class DbConnectionTest{
+    @Test
+    public void test(){
+        //assemble
+        var cut = new DbConnectionTest();
+        cut.test();
+        Assertions.assertEquals( 0,0);
+    }
 }
