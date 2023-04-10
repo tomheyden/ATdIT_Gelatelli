cc5ccedfd82f913328f9a8dc589a26469b8c04dc
tomheyden <Tom.Heyden@gmx.de>
2023-04-10 18:28:18 +0200

    Correction and Tests

--- Implementation/src/test/java/atdit/gelatelli/DbConnectionTest.java
+++ Implementation/src/test/java/atdit/gelatelli/DbConnectionTest.java
@@ -1,22 +1,38 @@
 package atdit.gelatelli;
 
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import java.util.*;
 
 /**
  * Unit test for simple App.
  */
 
 public class DbConnectionTest{
+
     @Test
-    public void test(){
-        //assemble
-        var cut = new DbConnectionTest();
-        cut.test();
-        Assertions.assertEquals( 0,0);
+    public void testConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/eiscafegelatelli", "root", "password");
+        Assertions.assertNotNull(connection);
+    }
+    
+    @Test
+    public void testreadfromDBtoWE() throws SQLException {
+       WarehouseService warehouseService = new WarehouseService();
+       List<Ingredient> actualIngredients = warehouseService.readfromDBtoWE(null);
+
+        List<Ingredient> expectedIngredients = new ArrayList<>();
+        expectedIngredients.add(new Ingredient("Cocoa powder",9.99,"kg"));
+        expectedIngredients.add(new Ingredient("Oreo",9.99,"kg"));
+        expectedIngredients.add(new Ingredient("Strawberry",5.02,"kg"));
+        expectedIngredients.add(new Ingredient("Vanilla extract",9.97,"l"));
+
+       Assertions.assertEquals(expectedIngredients,actualIngredients);
     }
 }
