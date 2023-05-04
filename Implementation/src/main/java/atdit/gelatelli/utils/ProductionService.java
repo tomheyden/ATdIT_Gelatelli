093985e4483a9480384ad686cae5e8451420f929
tomheyden <Tom.Heyden@gmx.de>
2023-05-04 19:35:14 +0200

    Changed db.properties and dependencies to MariaDB

--- Implementation/src/main/java/atdit/gelatelli/utils/ProductionService.java
+++ Implementation/src/main/java/atdit/gelatelli/utils/ProductionService.java
@@ -168,7 +168,6 @@ public class ProductionService implements ProductionInterface {
                             break;
                         }
                     } else {
-                        // Handle the case where no rows are returned
                     }
                 }
             }
