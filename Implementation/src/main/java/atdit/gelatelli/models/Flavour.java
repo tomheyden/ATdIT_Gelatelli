463d44718df4cbb1f78945203f46a0ec25123cb0
tomheyden <Tom.Heyden@gmx.de>
2023-05-08 00:11:58 +0200

    Cleaning of Code and Deletion of unnused methods

--- Implementation/src/main/java/atdit/gelatelli/models/Flavour.java
+++ Implementation/src/main/java/atdit/gelatelli/models/Flavour.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Represents a icecream flavour.
  */
-public class Flavour implements Comparable<Flavour> {
+public class Flavour {
 
     private String flavourName;
     private double contributionMargin;
@@ -58,54 +58,6 @@ public class Flavour implements Comparable<Flavour> {
     public String getFlavourName() {
         logger.debug("Retrieving the name of the Flavour instance");
         return flavourName;
-    }
-
-
-    /**
-     * Gets the earliest best-before date of the flavour.
-     *
-     * @return the earliest best-before date of the flavour
-     */
-    public LocalDate getEarliestBBD() {
-        logger.debug("Retrieving the earliest best before date of the Flavour instance");
-        return earliestBBD;
-    }
-
-    /**
-     * Increases the sort value of the flavour by 1.
-     */
-    public void increaseSort() {
-        sort++;
-    }
-
-    /**
-     * Compares this Flavour to another Flavour based on the earliestBBD attribute, then the sort attribute,
-     * and finally the contributionMargin attribute. Returns a negative integer, zero, or a positive integer
-     * as this Flavour is less than, equal to, or greater than the specified Flavour.
-     *
-     * @param other the Flavour to be compared.
-     * @return a negative integer, zero, or a positive integer as this Flavour is less than, equal to, or greater than the specified Flavour.
-     */
-    @Override
-    public int compareTo(Flavour other) {
-        // Compare by the 'earliestBBD' attribute first
-        if (earliestBBD != null && other.getEarliestBBD() != null) {
-            int bbdComparison = this.earliestBBD.compareTo(other.earliestBBD);
-            if (bbdComparison != 0) {
-                logger.debug("Comparing Flavour objects by earliestBBD: {} vs {}", this.earliestBBD, other.getEarliestBBD());
-                return bbdComparison;
-            }
-        }
-        // If 'earliestBBD' attributes are equal, compare by the 'sort' attribute
-        int sortComparison = Integer.compare(other.sort, this.sort);
-
-        // If 'sort' attributes are equal, compare by the 'contributionMargin' attribute
-        if (sortComparison == 0) {
-            logger.debug("Comparing flavours by contributionMargin: {} vs. {}: {}", this, other, sortComparison);
-            return Double.compare(other.contributionMargin, this.contributionMargin);
-        }
-
-        return sortComparison;
     }
 
     /**
